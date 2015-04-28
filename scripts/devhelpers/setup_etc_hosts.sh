ed3e74c27db4761da3f049d7d7b6ad53034ec99a
Oriol Fitó <oriol@akvo.org>
2015-04-28 17:30:08 +0200

    [#1] Fixing access to variable

--- scripts/devhelpers/setup_etc_hosts.sh
+++ scripts/devhelpers/setup_etc_hosts.sh
@@ -4,7 +4,7 @@ echo "This script will edit /etc/hosts and therefore requires your administrator
 
 # check if /etc/hosts last character is a newline or not
 lastline=$(tail -c 1 /etc/hosts)
-if [ "lastline" != "" ]; then
+if [ "$lastline" != "" ]; then
     sudo bash -c 'echo   \ \ >> /etc/hosts'
 fi
 sudo bash -c 'echo "192.168.50.101 unilog.localdev.akvo.org   # localdev_unilog" >> /etc/hosts'
