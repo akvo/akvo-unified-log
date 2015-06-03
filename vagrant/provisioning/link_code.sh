e9d6d525167684783bc1565750bc967833cc6716
Oriol Fitó <oriol@akvo.org>
2015-06-03 17:24:39 +0200

    [akvo/akvo-infrastructure#88] Removing the use of akvo-core-services repository when provisioning vagrant box

--- vagrant/provisioning/link_code.sh
+++ vagrant/provisioning/link_code.sh
@@ -8,8 +8,3 @@ sudo -H -u unilog ln -s /vagrant/unilog/checkout /var/akvo/unilog/code
 # linking akvo-flow-server-config into `/var/akvo/unilog/akvo-flow-server-config`
 rm -rf /var/akvo/unilog/akvo-flow-server-config
 sudo -H -u unilog ln -s /vagrant/flow-server-config/checkout /var/akvo/unilog/akvo-flow-server-config
-
-# cloning akvo-core-services to have the flow data schema
-if [ ! -d /var/akvo/unilog/akvo-core-services ]; then
-  git clone -b unified-log https://github.com/akvo/akvo-core-services.git /var/akvo/unilog/akvo-core-services
-fi
