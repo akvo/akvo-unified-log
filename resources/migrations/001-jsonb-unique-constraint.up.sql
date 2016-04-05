9bbda8c5fe6f7742205fcdddaea3451d09ea2d99
Jonas Enlund <jonas.enlund@gmail.com>
2016-04-05 21:51:23 +0300

    [#20] Drop duplicates before migrating to unique constraint

--- resources/migrations/001-jsonb-unique-constraint.up.sql
+++ resources/migrations/001-jsonb-unique-constraint.up.sql
@@ -1,2 +1,10 @@
 ALTER TABLE event_log DROP CONSTRAINT IF EXISTS event_log_payload_key;
+
+-- Drop duplicates
+DELETE FROM event_log
+  WHERE id IN (
+    SELECT id FROM (SELECT id, row_number() OVER (PARTITION BY payload ORDER BY id) AS rnum FROM event_log) t
+      WHERE t.rnum > 1
+  );
+
 CREATE UNIQUE INDEX unique_payload ON event_log (md5(payload::text));
