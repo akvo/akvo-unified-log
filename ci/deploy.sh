58ac04a6a3f281a6f1f69dc231be8eba1b2fdded
Dan Lebrero <dlebrero@gmail.com>
2018-12-27 16:40:00 +0100

    [#28] Publishing containers to docker registry

---
+++ ci/deploy.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+set -eu
+
+function log {
+   echo "$(date +"%T") - INFO - $*"
+}
+
+export PROJECT_NAME=akvo-lumen
+
+if [[ "${TRAVIS_BRANCH}" != "develop" ]] && [[ "${TRAVIS_BRANCH}" != "master" ]]; then
+    exit 0
+fi
+
+if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
+    exit 0
+fi
+
+log Making sure gcloud and kubectl are installed and up to date
+gcloud components install kubectl
+gcloud components update
+gcloud version
+which gcloud kubectl
+
+log Authentication with gcloud and kubectl
+gcloud auth activate-service-account --key-file ci/gcloud-service-account.json
+gcloud config set project akvo-lumen
+gcloud config set container/cluster europe-west1-d
+gcloud config set compute/zone europe-west1-d
+gcloud config set container/use_client_certificate True
+
+ENVIRONMENT=test
+if [[ "${TRAVIS_BRANCH}" == "master" ]]; then
+    log Environment is production
+    gcloud container clusters get-credentials production
+    ENVIRONMENT=production
+    POD_CPU_REQUESTS="400m"
+    POD_CPU_LIMITS="10000m"
+    POD_MEM_REQUESTS="5120Mi"
+    POD_MEM_LIMITS="5632Mi"
+else
+    log Environment is test
+    gcloud container clusters get-credentials test
+    POD_CPU_REQUESTS="200m"
+    POD_CPU_LIMITS="400m"
+    POD_MEM_REQUESTS="300Mi"
+    POD_MEM_LIMITS="300Mi"
+fi
+
+log Pushing images
+gcloud docker -- push eu.gcr.io/${PROJECT_NAME}/akvo-unilog
+
+log Deploying
+
+#sed -e "s/\$TRAVIS_COMMIT/$TRAVIS_COMMIT/" \
+#  -e "s/\${ENVIRONMENT}/${ENVIRONMENT}/" \
+#  -e "s/\${POD_CPU_REQUESTS}/${POD_CPU_REQUESTS}/" \
+#  -e "s/\${POD_MEM_REQUESTS}/${POD_MEM_REQUESTS}/" \
+#  -e "s/\${POD_CPU_LIMITS}/${POD_CPU_LIMITS}/" \
+#  -e "s/\${POD_MEM_LIMITS}/${POD_MEM_LIMITS}/" \
+#  ci/test/akvo-flow-services.yaml.template > akvo-flow-services.yaml
+#
+#kubectl apply -f akvo-flow-services.yaml
+#
+#ci/test/wait-for-k8s-deployment-to-be-ready.sh
+
+#docker-compose -p akvo-flow-ci -f docker-compose.yml -f docker-compose.ci.yml run --no-deps tests /import-and-run.sh kubernetes-test
+
+log Done
