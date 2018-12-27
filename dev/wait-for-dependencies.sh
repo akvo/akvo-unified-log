74b59c335bc7f95120f687cbcfe6305a2283a70c
Dan Lebrero <dlebrero@gmail.com>
2018-12-27 16:15:24 +0100

    [#28] Waiting for PG cert to be generated
    The test container is starting before the PG container generates the
    certificates

--- dev/wait-for-dependencies.sh
+++ dev/wait-for-dependencies.sh
@@ -3,12 +3,19 @@
 set -eu
 
 MAX_ATTEMPTS=120
+ATTEMPTS=0
 
 echo "Waiting for PostgreSQL ..."
 
+while [[ ! -f "${PGSSLROOTCERT}" && "${ATTEMPTS}" -lt "${MAX_ATTEMPTS}" ]]; do
+    echo "Waiting for certificate to be generated..."
+    sleep 1
+    let ATTEMPTS+=1
+done
+
 if [[ ! -d "/root/.postgresql" ]]; then
-    mkdir /root/.postgresql
-    cp "${PGSSLROOTCERT}" /root/.postgresql/root.crt
+     mkdir /root/.postgresql
+     cp "${PGSSLROOTCERT}" /root/.postgresql/root.crt
 fi
 
 ATTEMPTS=0
